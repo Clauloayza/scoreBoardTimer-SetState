c24be7323864bb940c76de0c57926744289a017b
claudia <clou.7loymed@gmail.com>
2017-10-05 15:37:05 -0500

    moviendo datos

--- app.jsx
+++ app.jsx
@@ -1,69 +1,66 @@
-const PLAYERS = [
-  {
-    name: "Jim Hoskins",
-    score: 31,
-    id: 1,
-  },
-   {
-    name: "Andree Hoskins",
-    score: 35,
-    id: 2,
-  },
-   {
-    name: "Alena Hoskins",
-    score: 42,
-    id: 3,
-  },
-];
-
+//scoreBmvc
 class Model {
-  constructor(players){
-    this.players = players;
+  constructor(){
+    this.players = [
+      {
+        name: "Jim Hoskins",
+        score: 31,
+        id: 1,
+      },
+       {
+        name: "Andree Hoskins",
+        score: 35,
+        id: 2,
+      },
+       {
+        name: "Alena Hoskins",
+        score: 42,
+        id: 3,
+      },
+    ];
     this.inputValue = null;
   }
 
   subscribe(render){
-    this.render = render;
+    this.callback = render;
   }
-  notify(){
-    this.render();
-  }
-  getPlayer(contenido){
-    this.players.push({
-      name: contenido.value,
-      score: 0,
-      id: this.players.length + 1
-    })
 
-    contenido.value = '';
-    this.notify();
+  notify(){
+    this.callback();
   }
-  getSum(player){
-    player.score++;
+
+  NewPlayer(name){
+      this.players.push({
+        name: this.input.value,
+        score:0,
+      })
+      this.input.value = '';
+    
     this.notify();
+    this.callback();
+  }
+
+  getPlayer(){
+    return model.players.length;
+  }
+
+  getSum(player){
+    this.players[player].score++;
+    this.notify();
+    this.callback();
   }
   getRest(player){
-    player.score--;
+    this.players[player].score--;
     this.notify();
+    this.callback();
   }
+
   getAllPoints(){
-    return this.players.map(item => item.score).reduce((total, item) => total + item);
-      
-  }
-}
-
-/*function AllPoints(players){
+    return model.players.map(player => player.score).reduce((a, b) => a + b);
+     /*function AllPoints(players){
       return players.map(player => player.score).reduce((a, b) => a + b);
-    }*/
-
-const Application = ({ model}) => {
-   return (
-     <div className="container scoreboard">
-        <Header model={model}/>
-        <PlayerList model={model}/>
-        <PlayerForm model = {model}/>  
-     </div>
-  );
+    }*/ 
+  }
 }
 
 const Header = ({model}) => {
@@ -72,7 +69,7 @@ const Header = ({model}) => {
         <table className="stats">
             <tr>
               <td>PLAYERS:</td>
-              <td>{model.players.length}</td>
+              <td>{model.getPlayer()}</td>
             </tr>
             <tr>
               <td>TOTAL POINTS:</td>
@@ -92,53 +89,56 @@ const Header = ({model}) => {
 
 const PlayerList = ({model}) => {
   return (
-      <div>
-        {model.players.map(e => {
-          return <Player e={e}/>
-        })}
-      </div>
-  );
-}
-
-const Player = ({player}) => {
-  return (
     <div>
       {
-        players.map((player) => {
-          return <div className='player' key={player.id}>
-            <div className='player-name'>{player.name}</div>
+        model.players.map((dato, player) => {
+          return (<div className='player'>
+            <div className='player-name'>{dato.name}</div>
             <div className='player-score counter'>
-              <button className='counter-action decrement' onClick={player.score ? () => model.getRest(player):''}>-</button>
-              <span className='counter-score'>{player.score}</span>
-              <button className='counter-action increment'onClick={() => model.getSum(e)}>+</button>
+              <button className='counter-action decrement' onClick={() => model.getRest(player)}>-</button>
+              <span className='counter-score'>{dato.score}</span>
+              <button className='counter-action increment'onClick={() => model.getSum(player)}>+</button>
             </div>
-          </div>
+          </div>)
         })
       }
     </div>
   );
 }
 
-const PlayerForm = ({model}) => {
+const PlayerForm = React.createClass({
+  render: function(){
+    return (
+      <div className="add-player-form">
+        <form action="" onSubmit={e => {e.preventDefault();
+        model.NewPlayer(name);
+        }}>
+          <input type="text" placeholder="Enter a name" onChange={e => (model.input = e.target)}/>
+          <input type="submit" placeholder="Add Player"/>
+        </form>
+      </div>
+    )
+  }
+})
+
+const Application = ({ title, model}) => {
   return (
-    <div className="add-player-form">
-      <form action="" onSubmit={e => {e.preventDefault();
-      model.getPlayer(model.inputValue);
-      }}
-      >
-        <input type="text" placeholder="Enter a name" onChange={e => (model.inputValue = e.target)}/>
-        <input type="submit" placeholder="Add Player"/>
-      </form>
+    <div className="container scoreboard">
+       <Header model={model}/>
+       <PlayerList model={model}/>
+       <PlayerForm/>  
     </div>
-  )
+ );
 }
 
-let model = new Model(PLAYERS);
+let model = new Model();
 
 let render = () =>{
-  ReactDOM.render(<Application model = {model}/>,
+  ReactDOM.render(<Application title="Scoreboard" model = {model}/>,
   document.getElementById('container'));
 }
 
 model.subscribe(render);
 render();
+
+
